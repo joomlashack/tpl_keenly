90635a001464c62065542fe9d9d24ef1b4d3f416
jmarkos <markos@joomlashack.com>
2014-12-18 15:26:30 -0600

    removing index php file and adding functions and thumbnail files

---
+++ code/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* @package Keenly
+* @subpackage Functions
+*
+* @copyright Copyright (C) 2005 - 2014 Joomlashack. Meritage Assets. All rights reserved.
+* @license GNU General Public License version 2 or later; see LICENSE.txt
+*/
+// Restrict Access to within Joomla
+defined('_JEXEC') or die('Restricted access');
