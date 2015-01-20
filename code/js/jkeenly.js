70d45186d1d4a8f86b7ca70781a7c32d00658df1
Roberto <roberto@joomlashack.com>
2015-01-20 18:38:13 +0100

    TPLKEENLY-102 #comment Custom html tags does´t work on IE.

--- code/js/jkeenly.js
+++ code/js/jkeenly.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
     $totalMenu = jQuery('.moduletable_ktotalmenu').children('.menu').children();
     jQuery('.total-menu-wrapper').append('<div class="total-menu-container"><div class="menu-close"><i class="icon-remove"></i></div><div class="menu-items"><div class="wrapper-items"></div></div><div class="submenu-items"><div class="wrapper-submenu-items"></div></div></div>');
-    
+
     // Rendering menu 
     jQuery($totalMenu).each(function() {
         if (jQuery(this).hasClass('parent')) {
@@ -10,20 +10,21 @@ jQuery(document).ready(function($) {
             jQuery($menuParent).each(function() {
                 if (jQuery(this).is('a')) {
                     $menuItemElement = jQuery(this).clone().appendTo('.wrapper-items').attr('href', jQuery(this).attr('href') + '-total-menu').bind('click', itemMenuClickEvent);
-                    if (jQuery(this).parent().hasClass('active')) {
+                    if (checkingClass(jQuery(this).parent(), 'active')) {
                         $menuItemElement.addClass('active');
                     }
                 } else {
                     $itemCloneElement = jQuery(this).clone().appendTo('.wrapper-submenu-items').attr('id', jQuery(this).attr('id') + '-total-menu');
                     if (jQuery(this).hasClass('in')) {
-                        $itemCloneElement.attr('active', 'true');
+                        $itemCloneElement.addClass('active-menu');
                     }
+
                 }
             });
         } else {
             $linkItemMenu = jQuery(this).children('a').clone().appendTo('.wrapper-items');
-            if (jQuery(this).hasClass('active')) {
-            	 $linkItemMenu.addClass('active');
+            if (checkingClass(jQuery(this), 'active')) {
+                $linkItemMenu.addClass('active');
             }
         }
     });
@@ -31,6 +32,13 @@ jQuery(document).ready(function($) {
     // Bing open and close functions
     jQuery('.menu-close').bind('click', closeMenu);
     jQuery('.total-menu-btn').bind('click', openMenu);
+
+    function checkingClass(element, checkClass) {
+        if (jQuery(element).hasClass(checkClass)) {
+            return true;
+        }
+        return false;
+    }
 
     // Action functions 
     function openMenu() {
@@ -43,11 +51,10 @@ jQuery(document).ready(function($) {
 
     // Selected item detects the menu that is open close it and open the corresponding item.
     function itemMenuClickEvent() {
-        jQuery('*[active="true"]').removeClass('in');
-        jQuery('*[active="true"]').height(0);
-        jQuery('*[active="true"]').removeAttr('active');
+        jQuery('.active-menu').removeClass('in');
+        jQuery('.active-menu').height(0);
+        jQuery('.active-menu').removeClass('active-menu');
         $menuSubmenuItem = jQuery(this).attr('href');
-        console.log($menuSubmenuItem);
-        jQuery($menuSubmenuItem).attr('active', 'true');
+        jQuery($menuSubmenuItem).addClass('active-menu');
     }
 });
