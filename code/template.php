5ed6bf253b3c9032bc853a5eedadca06ae3bcb2a
jmarkos <jkmarcosb@gmail.com>
2016-06-30 10:50:24 -0600

    merege of master

--- code/template.php
+++ code/template.php
@@ -19,8 +19,8 @@ defined('_JEXEC') or die('Restricted access');
 <html>
 <head>
 	<w:head />
-	<link href='http://fonts.googleapis.com/css?family=Karma:300,400,600' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Karma:300,400,600' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 </head>
 <body class="<?php echo $wrightBodyClass ?>">
 	<div class="frame"></div>
